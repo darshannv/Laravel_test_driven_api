f70bfc42be4b2a2e6b8289c56f659b8df42fb9b6
darshannv <darshanvasu3048@gmail.com>
2023-06-21 19:36:12 +0530

    task api update

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class TaskController extends Controller
 {
     public function index() {
         $task = Task::all();
         return response($task);
+    }
+
+    public function store(Request $request){
+
+        $task = Task::create($request->all());
+        return response($task, 201);
+    }
+
+    public function destroy(Task $task) {
+
+        $task->delete();
+        return response('', Response::HTTP_NO_CONTENT);
     }
 }
--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Task;
+use Illuminate\Routing\Route;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -14,7 +15,7 @@ class TaskTest extends TestCase
     public function test_fetch_all_tasks_of_a_todo_list()
     {
         //Preparation
-        $task = Task::factory()->create();
+        $task = $this->createTask();
 
         //action
         $response = $this->getJson(route('task.index'))->assertOk()->json();
@@ -22,5 +23,28 @@ class TaskTest extends TestCase
         //assertion
         $this->assertEquals(1, count($response));
         $this->assertEquals($task->title, $response[0]['title']);
+    }
+
+
+    public function test_store_a_task_for_todo_list(){
+
+
+        $task = $this->createTask();
+
+        $this->postJson(route('task.store'), ['title' => $task->title])
+            ->assertCreated();
+
+        
+        $this->assertDatabaseHas('tasks', ['title' => $task->title]);
+        
+    }
+
+    public function test_delete_a_task_from_database(){
+
+        $task = Task::factory()->create();
+
+        $this->deleteJson(Route('task.destroy', $task->id))->assertNoContent();
+
+        $this->assertDatabaseMissing('tasks', ['title' => $task->title]);
     }
 }
